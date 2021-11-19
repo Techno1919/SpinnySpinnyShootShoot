8f8d3a3de2949bedae76823414348f07a0a99e58
Techno1919 <61212536+Techno1919@users.noreply.github.com>
2021-11-19 09:56:39 -0700

    Updated Player
    Changed the invincible code to look a little better

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -123,11 +123,7 @@ public class Player : MonoBehaviour
     {
         if(collision.gameObject.tag == "EnemyShot")
         {
-            if(invincible)
-            {
-            
-            }
-            else
+            if(!invincible)
             {
                 if (halfHeartLeft)
                 {

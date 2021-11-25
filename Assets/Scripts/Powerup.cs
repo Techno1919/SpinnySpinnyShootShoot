548b611e5a7251401660010dffe52f25c0f665d8
Alex McDowell <scruffyalexhs@gmail.com>
2021-11-24 19:39:00 -0700

    trying to fix powerups

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -26,14 +26,14 @@ public class Powerup : MonoBehaviour
 
     private void Update()
     {
-        if(wimzard.powerActive)
+        if (wimzard.powerActive)
         {
             if (wimzard.timeRemaining > 0)
             {
                 //timeRemaining -= Time.deltaTime;
                 wimzard.timeRemaining -= Time.deltaTime;
             }
-            else if (wimzard.timeRemaining < 0)
+            else if (wimzard.timeRemaining <= 0)
             {
                 powerDown.Play();
                 wimzard.timeRemaining = 0;
@@ -42,17 +42,17 @@ public class Powerup : MonoBehaviour
                 wimzard.shot.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
                 wimzard.invincible = false;
                 wimzard.powerActive = false;
-                 
+
             }
         }
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
-            
+
             wimzard.shot.speed = 7;
             wimzard.shot.damage = 25;
             wimzard.shot.lifetime = 5;
@@ -60,39 +60,41 @@ public class Powerup : MonoBehaviour
             switch (powerupName)
             {
                 case "FastFire":
-                    pickUpNoise.Play();
                     wimzard.powerActive = true;
                     wimzard.shotTimer = 0.1f;
                     wimzard.shot.speed = 100;
                     wimzard.timeRemaining = 5;
                     timeRemaining = 5;
-                    
+
                     break;
                 case "BiggerBullet":
-                    pickUpNoise.Play();
                     wimzard.powerActive = true;
                     wimzard.shot.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
                     timeRemaining = 6;
                     wimzard.timeRemaining = 6;
-                    
+
                     break;
                 case "MarioStarRipoff":
-                    pickUpNoise.Play();
                     wimzard.powerActive = true;
                     wimzard.invincible = true;
                     wimzard.timeRemaining = 10;
-                    
+
                     timeRemaining = 10;
                     break;
-                   
             }
+            PlayPowerUp();
             Debug.Log("Pickup");
             Destroy(gameObject);
+            }
         }
+
+    public void PlayPowerUp()
+    {
+        pickUpNoise.Play();
+        Debug.Log("Pickup noise played");
     }
 
-
-   
-       
-
 }
+
+ 
+
